6c1b0f5b329fcf439d9edd23ff5ea7994b33c11c
Asr13b <fedlukedirasrar@gmail.com>
2025-10-21 10:40:06 +0300

    modify request part

--- netflix-clone/src/utils/Requests.jsx
+++ netflix-clone/src/utils/Requests.jsx
@@ -2,14 +2,13 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+fetchAnimationNature: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=16&with_keywords=nature,wildlife,planet,animals&without_keywords=people,human,biography&sort_by=popularity.desc`,
+  
+  fetchNatureDocumentaries: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99&with_keywords=nature,wildlife,planet,animals&without_keywords=people,human,biography&sort_by=popularity.desc`,
+
+  fetchTrending: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=16,99&with_keywords=nature,wildlife,planet,animals&without_keywords=people,human,biography&sort_by=popularity.desc`,
+
+  fetchTopRated: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=16,99&with_keywords=nature,wildlife,planet,animals&without_keywords=people,human,biography&sort_by=vote_average.desc`,
 };
 
 export default requests;
